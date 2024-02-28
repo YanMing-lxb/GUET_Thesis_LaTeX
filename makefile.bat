0bc127d9058e540a2cb344f0c46c22052280d26c
焱铭 <lxb-yanming@foxmail.com>
2024-02-28 18:16:28 +0800

    使用makefile输入命令，如终端输入 makefile -p -nq main

--- makefile.bat
+++ makefile.bat
@@ -1,2 +1,15 @@
 @echo off
-  python pytexmk.py
+
+REM 拼接参数为一个字符串
+set "args=%~1"
+:loop
+shift
+if "%~1"=="" goto :continue
+set "args=%args% %1"
+goto :loop
+:continue
+
+REM 运行 makefile
+python ./pytexmk.py %args%
+
+@REM 示例：makefile 使用默认参数运行，makefile -p -nq main' 使用 pdflatex 非安静模式编译 
